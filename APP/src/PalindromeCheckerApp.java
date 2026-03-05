a17fe7d46414ab9826b11968c03323abed9c063b
vs7295 <vs7295@srmist.edu.in>
2026-03-05 15:24:08 +0530

    added uc2

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -8,7 +8,8 @@ public class PalindromeCheckerApp {
         // Loop only till half of the string length
         for (int i = 0; i < input.length() / 2; i++) {
 
-            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
+            if(input.charAt(i) != input.charAt(input.length() - i - 1)) {
+
                 isPalindrome = false;
                 break;
             }
